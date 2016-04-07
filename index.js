34c646ee9d6d79cf8bcd380b5de1c65f9f45f1a1
Aris Setyono <aris@Aris-Setyono.local>
2016-04-07 14:53:36 +0700

    step 2

--- index.js
+++ index.js
@@ -3,11 +3,32 @@
 var http = require('http');
 var hostname = '127.0.0.1';
 var port = 1337;
+var pages = {
+	home:'Home page',
+	about:'About page',
+	blog:'Blog page',
+};
 
 var run = http.createServer(function(req, res) {
-	console.log('one request');
-	res.writeHead(200, { 'Content-Type': 'text/plain' });
-	res.end('Hello World\n');
+	var page = 'Page not found';
+	var code = 404;
+
+	if(req.url === '/home' || req.url === '/'){
+		page = pages.home;
+		code = 200;
+	}
+	else if(req.url === '/blog'){
+		page = pages.blog;
+		code = 200;
+	}
+	else if(req.url === '/about'){
+		page = pages.about;
+		code = 200;
+	}
+
+	var json = JSON.stringify({page:page});
+	res.writeHead(code, {'Content-Type': 'application/json'});
+	res.end(json);
 });
 
 run.listen(port, hostname, function() {
---
+++ models/users.js
@@ -0,0 +1,10 @@
+// models/users.js
+var users = function() {
+	return {
+		get:function() {
+			return 'dari user model';
+		}
+	}
+};
+
+module.exports = users;
